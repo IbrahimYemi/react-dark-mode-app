0d0a89e03944f05c31d238bceb93e5d2ab543e20
IbrahimYemi <sharafadeen.might@gmail.com>
2022-06-05 15:05:08 +0100

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import Body from './Body';
+import data from './data';
 import './style.css';
 
 export default function App() {
   const [darkMode, setDarkMode] = React.useState(true);
+  const cards = data.map((item) => {
+    return (
+      <div key={item.id} className="card">
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
 
   function toggleDarkMode() {
     setDarkMode((prevMode) => !prevMode);
@@ -14,7 +23,7 @@ export default function App() {
     <div className="container">
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Body darkMode={darkMode} />
-      <div className="card-row"></div>
+      <div className="card-row">{cards}</div>
     </div>
   );
 }
