895f2c957501bf2114cbfb09647783b6d458c878
IbrahimYemi <sharafadeen.might@gmail.com>
2022-06-05 15:30:37 +0100

    3 files modified

--- Body.js
+++ Body.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import data from './data';
 
 export default function Main(props) {
+  const cards = data.map((item) => {
+    return (
+      <div key={item.id} className={props.darkMode ? 'darkcard' : 'card'}>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
+
   return (
     <main className={props.darkMode ? 'dark' : ''}>
       <h1 className="main--title">WHAT SHOULD I DO WITH REACT</h1>
@@ -11,6 +21,7 @@ export default function Main(props) {
         <li>Next js will also be added</li>
         <li>Be so good you'll always be wanted</li>
       </ul>
+      <div className="card-row">{cards}</div>
     </main>
   );
 }
